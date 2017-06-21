e813c1c13374cbc55e7c19a1ca89e60a9fd7bb32
clapette <clapette01@gmail.com>
2017-06-21 11:32:50 +0200

    ntm benyaya

--- app/src/main/java/com/example/aifedespaix/applicationnotee/Main3Activity.java
+++ app/src/main/java/com/example/aifedespaix/applicationnotee/Main3Activity.java
@@ -1,5 +1,9 @@
 package com.example.aifedespaix.applicationnotee;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +13,27 @@ public class Main3Activity extends AppCompatActivity {
 
     Button btnOk;
     Button btnPasOk;
+    Button btnOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        btnOk = (Button) findViewById(R.id.buttonOk);
+        btnOption = (Button) findViewById(R.id.buttonOption);
+        btnOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                PackageManager pm = getPackageManager();
+                Intent intContacts = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                intContacts.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(intContacts, RESULT_OK);
+            }
+        });
 
+
+
+        btnOk = (Button) findViewById(R.id.buttonOk);
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
